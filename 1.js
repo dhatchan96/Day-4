51d02aa91aeac16f526ef4d85b87f8390f03b291
dhatchan96 <dhatchan0@gmail.com>
2024-02-20 10:30:45 +0530

    Initial commit

---
+++ 1.js
@@ -0,0 +1,51 @@
+// To create own resume in JSON format
+
+let resume = {
+    "personal_information": {
+      "name": "S Dhatchanamoorthy",
+      "email": "dhatchxxx@gmail.com",
+      "phone": "123-456-7890",
+      "address": "123 Main Street, Cityville, Chennai, 600228"
+    },
+    "education": [
+      {
+        "degree": "Bachelor of Science in Computer Science",
+        "university": "Pondicherry University",
+        "location": "Pondicherry, Pondicherry",
+        "graduation_year": "2018"
+      }
+    ],
+    "experience": [
+      {
+        "position": "Software Engineer",
+        "company": "US Bank",
+        "location": "Chennai, Tamil Nadu",
+        "start_date": "Nov 2020",
+        "end_date": "Present",
+        "responsibilities": [
+          "Developing and maintaining software applications.",
+          "Collaborating with team members to design and implement new features.",
+          "Troubleshooting and debugging issues in existing codebase."
+        ]
+      }
+    ],
+    "skills": [
+      "Programming Languages: Python, JavaScript",
+      "Web Development: HTML, CSS",
+      "Database Management: SQL",
+      "Version Control: Git",
+      "Problem Solving",
+      "Team Collaboration"
+    ],
+    "languages": [
+      {
+        "language": "Tamil",
+        "fluency": "Native"
+      },
+      {
+        "language": "English",
+        "fluency": "Intermediate"
+      }
+    ]
+  }
+  
